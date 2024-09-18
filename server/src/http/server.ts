9b9672d276197d30a18c52317e6429dd2ee896ce
MarlonVictor <cmarlonvictor11@gmail.com>
2024-09-18 20:19:27 -0300

    feat: rota create-goal com zod validator feita

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,6 +1,34 @@
 import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoal } from '../services/create-goal'
 
-const app = fastify()
+import z from 'zod'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async (req) => {
+    const { title, desiredWeeklyFrequency } = req.body
+
+    await createGoal({ title, desiredWeeklyFrequency })
+  }
+)
 
 app
   .listen({
