759c06f4f7e2be81b9cfea7f91549e6fd9d3d449
MarlonVictor <cmarlonvictor11@gmail.com>
2024-09-18 19:52:17 -0300

    feat: seed criado para inserir dados iniciais ao banco

---
+++ server/src/db/seed.ts
@@ -0,0 +1,28 @@
+import { client, db } from '.'
+import { goalCompletions, goals } from './schema'
+import dayjs from 'dayjs'
+
+async function seed() {
+  const startOfWeek = dayjs().startOf('week')
+
+  await db.delete(goalCompletions)
+  await db.delete(goals)
+
+  const [goal1, goal2, goal3] = await db
+    .insert(goals)
+    .values([
+      { title: 'Acordar cedo', desiredWeeklyFrequency: 5 },
+      { title: 'Me exercitar', desiredWeeklyFrequency: 3 },
+      { title: 'Meditar', desiredWeeklyFrequency: 1 },
+    ])
+    .returning()
+
+  await db.insert(goalCompletions).values([
+    { goalId: goal1.id, createdAt: startOfWeek.toDate() },
+    { goalId: goal2.id, createdAt: startOfWeek.add(8, 'hour').toDate() },
+  ])
+}
+
+seed().finally(() => {
+  client.end()
+})
